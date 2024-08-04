9192af5d92e1a1e29c65f905dcdb8985448fd4bd
zhihuat-jiaxing <zhihuat99@gmail.com>
2024-08-04 20:22:08 +0800

    add new attack

--- src/updater/__init__.py
+++ src/updater/__init__.py
@@ -4,3 +4,4 @@ from updater import SemiAsyncUpdater
 from updater import ActiveAsyncUpdater
 from updater import PersonalUpdater
 from updater import PoisonAsyncUpdater
+from updater import PoisonSyncUpdater
