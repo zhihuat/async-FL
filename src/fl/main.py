06ef1d8327dbbc0b18b431cd487ebc14691da517
zhihuat-jiaxing <zhihuat99@gmail.com>
2024-08-07 21:50:49 +0800

    Semantic and Edge Case

--- src/fl/main.py
+++ src/fl/main.py
@@ -32,18 +32,24 @@ def _read_data(dataset):
     return data, targets
 
 
-def send_dataset(train_dataset, test_dataset, message_queue, global_config):
+def send_dataset(train_dataset, test_dataset, poison_test_dataset, message_queue, global_config):
     # 预加载
     if 'dataset_pre_load' in global_config and global_config['dataset_pre_load']:
         data, targets = _read_data(train_dataset)
         message_queue.set_train_dataset(CustomDataset(data, targets))
         data, targets = _read_data(test_dataset)
         message_queue.set_test_dataset(CustomDataset(data, targets))
+        if poison_test_dataset is not None:
+            data, targets = _read_data(poison_test_dataset)
+            message_queue.set_poison_test_dataset(CustomDataset(data, targets))
+        else:
+            message_queue.set_poison_test_dataset(None)
     # 静态加载
     else:
         message_queue.set_train_dataset(train_dataset)
         message_queue.set_test_dataset(test_dataset)
-    return train_dataset, test_dataset
+        message_queue.set_poison_test_dataset(poison_test_dataset)
+    return train_dataset, test_dataset, poison_test_dataset
 
 
 def generate_client_stale_list(global_config):
@@ -52,7 +58,8 @@ def generate_client_stale_list(global_config):
         client_staleness_list = stale
     elif isinstance(stale, bool):
         client_staleness_list = []
-        for i in range(global_config["client_num"]):
+        clients_num = get_client_num(global_config["client_num"])
+        for i in range(clients_num):
             client_staleness_list.append(0)
     elif isinstance(stale, dict) and "path" in stale:
         stale_generator = ModuleFindTool.find_class_by_path(stale["path"])()(stale["params"])
@@ -85,6 +92,7 @@ def main():
         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../config.json")
     else:
         config_file = sys.argv[1]
+    
 
     config = getJson(config_file)
     # 生成uuid
@@ -175,13 +183,20 @@ def main():
     dataset = dataset_class(global_config["client_num"], global_config["iid"], global_config["dataset"]["params"])
     train_dataset = dataset.get_train_dataset()
     test_dataset = dataset.get_test_dataset()
-    train_dataset, test_dataset = send_dataset(train_dataset, test_dataset, message_queue, global_config)
+    poison_test_dataset = dataset.get_poison_test_dataset()
+    train_dataset, test_dataset, poison_test_dataset = send_dataset(
+        train_dataset, test_dataset, poison_test_dataset, message_queue, global_config)
     index_list = dataset.get_index_list()
+    poison_index_list = dataset.get_poison_index_list()
     test_index_list = dataset.get_test_index_list()
+    poison_test_index_list = dataset.get_poison_test_index_list()
     client_manager_config["index_list"] = index_list
+    client_manager_config["poison_index_list"] = poison_index_list    
     global_var['client_index_list'] = index_list
+    global_var['poison_client_index_list'] = poison_index_list
     global_var['test_index_list'] = test_index_list
-
+    global_var['test_poison_index_list'] = poison_test_index_list
+    
     # 启动client_manager
     # get the running mode of client
     running_mode(config)
